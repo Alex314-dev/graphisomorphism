4442487cce6a79da82222d247217dcaf87168690
Kagan <khagan.gulsum@gmail.com>
2022-03-12 15:33:34 +0100

    Fix empty alpha bug

--- color_refinement.py
+++ color_refinement.py
@@ -18,13 +18,17 @@ def color_refinement(D: ["Vertex"], I: ["Vertex"], G: "Graph"):
         i = i + 1  # the next available coloring
     else:
         for v in G.vertices:
-            v.colornum = 0
+            i = max(i, v.colornum)
         for x in range(0, len(D)):
-            vertexD = D[x]
-            vertexI = I[x]
-            vertexD.colornum = x + 1
-            vertexI.colornum = x + 1
-        i = len(D) + 1  # the next available coloring
+            i = i + 1
+            D[x].colornum = i
+            I[x].colornum = i
+        for v in G.vertices:
+            if v.colornum in alpha.keys():
+                alpha[v.colornum].append(v)
+            else:
+                alpha[v.colornum] = [v]
+        i = i + 1  # the next available coloring
 
     while prev_i != i:
         if prev_i != -1:
@@ -134,7 +138,37 @@ def update_vertex_coloring(alpha: {int: ["Vertex"]}, i: int):
 #     print(output)
 #
 #
-# if __name__ == '__main__':
-#     with open("./SignOffColRefBackup/SignOffColRefBackup1.grl") as f:
-#         graph_list = read_graph_list(Graph, f)
-#     color_refinement_main(graph_list[0])
+if __name__ == '__main__':
+    G = Graph(False, 8)
+    H = Graph(False, 8)
+    Gv = sorted(list(G.vertices), key=lambda v: v.label)
+    Hv = sorted(list(H.vertices), key=lambda v: v.label)
+    G.add_edge(Edge(Gv[0], Gv[1]))
+    G.add_edge(Edge(Gv[0], Gv[5]))
+    G.add_edge(Edge(Gv[0], Gv[2]))
+    G.add_edge(Edge(Gv[1], Gv[4]))
+    G.add_edge(Edge(Gv[1], Gv[7]))
+    G.add_edge(Edge(Gv[2], Gv[3]))
+    G.add_edge(Edge(Gv[2], Gv[5]))
+    G.add_edge(Edge(Gv[3], Gv[4]))
+    G.add_edge(Edge(Gv[3], Gv[6]))
+    G.add_edge(Edge(Gv[4], Gv[7]))
+    G.add_edge(Edge(Gv[5], Gv[6]))
+    G.add_edge(Edge(Gv[6], Gv[7]))
+    H.add_edge(Edge(Hv[0], Hv[1]))
+    H.add_edge(Edge(Hv[0], Hv[6]))
+    H.add_edge(Edge(Hv[0], Hv[7]))
+    H.add_edge(Edge(Hv[1], Hv[2]))
+    H.add_edge(Edge(Hv[1], Hv[3]))
+    H.add_edge(Edge(Hv[2], Hv[3]))
+    H.add_edge(Edge(Hv[2], Hv[4]))
+    H.add_edge(Edge(Hv[3], Hv[5]))
+    H.add_edge(Edge(Hv[4], Hv[5]))
+    H.add_edge(Edge(Hv[4], Hv[6]))
+    H.add_edge(Edge(Hv[5], Hv[7]))
+    H.add_edge(Edge(Hv[6], Hv[7]))
+    U = G + H
+    print(color_refinement([], [], U))
+    Uv = sorted(list(U.vertices), key=lambda v: v.label)
+    alpha = color_refinement([Uv[0], Uv[5]], [Uv[8], Uv[15]], U)
+    print(alpha)
